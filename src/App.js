ab66f0bf667c1c19ff5ba33d3604ea86ff37ff1c
amansohal8654 <amansohal8654@gmail.co>
2021-09-24 12:49:30 +1200

    Setting up Crypto Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+
+const App = () => {
+  return (
+    <div className="App">
+        Hello world;
+    </div>
+  );
+}
+
+export default App;
