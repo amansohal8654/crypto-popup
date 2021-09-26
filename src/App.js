ff393173750ff7dc369e616700d0fe7edcd21165
amansohal8654 <amansohal8654@gmail.co>
2021-09-27 11:54:45 +1300

    get coins data from api call using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -31,10 +31,18 @@ const App = () => {
                 </Switch>
             </div>
           </Layout>
-        </div>
-        <div className="footer">
-
-        </div>
+          <div className="footer">
+            <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+              Crypto Pop Up <br />
+              All rights reserved.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
+        </div> 
     </div>
   );
 }
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '0ab624c47bmsh85ddc3b98a04210p1259d4jsn1eea87419413'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
